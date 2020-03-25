2e0d7decf7250a4f2964748f88cef515429ffd59
unknown <chloe.percherancier@yahoo.com>
2020-03-25 16:35:16 +0100

    ajout component et navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {MDBBootstrapModule} from 'angular-bootstrap-md';
 
+import { Routes, RouterModule } from '@angular/router';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { MedicamentsComponent } from './medicaments/medicaments.component';
+import { MedecinsComponent } from './medecins/medecins.component';
+import { VisitesComponent } from './visites/visites.component';
+import { AccueilComponent } from './accueil/accueil.component';
+
+const appRoutes: Routes = [
+  { path: 'connexion', component: ConnexionComponent},
+  { path: 'accueil', component: AccueilComponent},
+  { path: 'medicaments', component: MedicamentsComponent},
+  { path: 'medecins', component: MedecinsComponent},
+  { path: 'visites', component: VisitesComponent}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConnexionComponent,
+    MedicamentsComponent,
+    MedecinsComponent,
+    VisitesComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule,
